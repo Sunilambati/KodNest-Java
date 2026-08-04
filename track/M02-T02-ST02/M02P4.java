22693100e15b83b1329bbf680c5d8965c38bc53c
Ambati.Sunil <sunilambati.003@gmail.com>
2026-08-04 22:20:04 +0530

    M02P4

---
+++ track/M02-T02-ST02/M02P4.java
@@ -0,0 +1,33 @@
+
+public class M02P4 {
+
+    public static void main(String[] args) {
+        double principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+
+        double simpleInterest = principal * rate * time / 100.0;
+        double totalAmount = principal + simpleInterest;
+
+        double weight = 72.0;
+        double height = 1.8;
+
+        double bmi = weight / (height * height);
+
+        int java = 78;
+        int sql = 84;
+        int aptitude = 69;
+        int communication = 91;
+        int web = 88;
+
+        int totalMarks = java + sql + aptitude + communication + web;
+
+        double percentage = totalMarks * 100.0 / 500;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
+    }
+}
