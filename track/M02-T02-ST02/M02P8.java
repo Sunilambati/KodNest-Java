664f1b95f899b1915bc1051a671468a357be5776
Ambati.Sunil <sunilambati.003@gmail.com>
2026-08-05 11:31:35 +0530

    M02P8

---
+++ track/M02-T02-ST02/M02P8.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class M02P8 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int num = scan.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
