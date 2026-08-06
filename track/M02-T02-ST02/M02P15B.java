8128880f108366982fa87ff04797019e8e1762df
Ambati.Sunil <sunilambati.003@gmail.com>
2026-08-06 12:26:47 +0530

    M02P15B

---
+++ track/M02-T02-ST02/M02P15B.java
@@ -0,0 +1,12 @@
+
+public class M02P15B {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            if (i == 6) {
+                break;
+            }
+        }
+    }
+}
