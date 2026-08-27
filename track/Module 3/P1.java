a9adc28b93015354968b16de126c467c7de3cead
Ambati.Sunil <sunilambati.003@gmail.com>
2026-08-27 12:10:41 +0530

    Main

---
+++ track/Module 3/P1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                System.out.print(" ");
+            }
+            System.out.print(scanner.nextInt());
+        }
+
+        scanner.close();
+    }
+}
